24b0baa7a9790e7b2c10fbb313ae04e2603cfb05
kyatomaru <souryu1010@gmail.com>
2025-05-06 23:33:13 +0900

    fix: 画像パスの指定エラーの修正

--- src/components/LoadingPage.tsx
+++ src/components/LoadingPage.tsx
@@ -6,7 +6,16 @@ export default function LoadingPage() {
     return (
         <Box sx={{ overflow: "hidden", position: "fixed", top: 0, display: 'flex', zIndex: 2200, width: "100%", maxWidth: "550px", height: "100vh", alignItems: "center", justifyContent: "center", backgroundColor: "white" }}>
             {/* <CircularProgress sx={{ zIndex: 2000 }} color="success" /> */}
-            <Image src="/small-icon.png" alt="loading" width={100} height={100} /> 
+            <span style={{ position: 'relative', width: '60px', height: '60px', display: 'inline-block' }}>
+                <Image 
+                    src="/images/icon.png" 
+                    alt="loading" 
+                    fill
+                    priority
+                    sizes="60px"
+                    style={{ objectFit: 'contain' }}
+                /> 
+            </span>
         </Box>
     );
 }
