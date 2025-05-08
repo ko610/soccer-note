2de2035d27eefd059d0adb7fb58c3906ce385b8e
kyatomaru <souryu1010@gmail.com>
2025-05-08 22:23:09 +0900

    feat: 画像のpost処理の追加

--- src/features/note/services/noteService.ts
+++ src/features/note/services/noteService.ts
@@ -1,8 +1,11 @@
 import { addDoc, collection, deleteDoc, doc, getDocs, serverTimestamp, updateDoc } from "firebase/firestore";
-import { db } from "@/lib/firebase/config";
+import { auth, db, storage } from "@/lib/firebase/config";
 import { NoteType } from "@/types/note/Note";
 import { GameModel } from "@/types/note/game/Game";
 import { PracticeModel } from "@/types/note/practice/Practice";
+import { getDownloadURL } from "firebase/storage";
+import { uploadBytes } from "firebase/storage";
+import { ref } from "firebase/storage";
 
 // ノートを取得する
 export const fetchNotes = async (): Promise<NoteType[]> => {
@@ -25,20 +28,35 @@ export const fetchNotes = async (): Promise<NoteType[]> => {
 };
 
 // ノートを作成する
-export const createNote = async (note: NoteType) => {
+export const createNote = async (note: NoteType, images: File[]) => {
+  const uid = await auth.currentUser?.uid;
+  if (!uid) {
+    throw new Error("User not found");
+  }
+
   const notesRef = collection(db, "notes");
+  const imageUrls = await uploadImage(images, uid);
   await addDoc(notesRef, {
     ...note,
+    uid: uid,
+    imageUrls: imageUrls,
     createDate: serverTimestamp(),
     updateDate: serverTimestamp(),
   });
 };
 
 // ノートを更新する
-export const updateNote = async (note: NoteType) => {
+export const updateNote = async (note: NoteType, images: File[]) => {
+  const uid = await auth.currentUser?.uid;
+  if (!uid) {
+    throw new Error("User not found");
+  }
+
   const notesRef = doc(db, "notes", note.id);
+  const imageUrls = await uploadImage(images, uid);
   await updateDoc(notesRef, {
     ...note,
+    imageUrls: imageUrls,
     updateDate: serverTimestamp(),
   });
 };
@@ -47,4 +65,22 @@ export const updateNote = async (note: NoteType) => {
 export const deleteNote = async (note: NoteType) => {
   const notesRef = doc(db, "notes", note.id);
   await deleteDoc(notesRef);
+};
+
+// 画像をアップロードする
+export const uploadImage = async (images: File[], uid: string): Promise<string[]> => {
+    const imageUrls: string[] = [];
+    for (let i = 0; images.length; i++) {
+        const file = images[i];
+        const fileName = `note/${uid}/${Date.now()}_${i}_${file.name}`;
+
+        const storageRef = ref(storage, fileName);
+        const fileBuffer = await file.arrayBuffer();
+        await uploadBytes(storageRef, new Uint8Array(fileBuffer));
+
+        const publicUrl = await getDownloadURL(storageRef);
+        imageUrls.push(publicUrl);
+    }
+
+    return imageUrls;
 };
